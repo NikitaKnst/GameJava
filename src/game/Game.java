5bb078f6cc55705ab3d323f0c27f9172f60f864c
Alebro <pupil@ivkhk.ee>
2021-09-08 10:43:16 +0300

    MyFirstCommit

---
+++ src/game/Game.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ *
+ * @author pupil
+ */
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Random random=new Random()
+                int RandomNumber=random.nextInt(20)
+                        System.out.println(RandomNumber);
+                 
+       while(RandomNumber != ){
+       } 
+       }
+    }
+    
+}

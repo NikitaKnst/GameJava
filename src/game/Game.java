e579209dd8eefe583b0efe0af4980eeae7913c17
pupil <pupil@ivkhk.ee>
2021-09-13 09:44:42 +0300

    Secondt

--- src/game/Game.java
+++ src/game/Game.java
@@ -20,6 +20,7 @@ public class Game {
            Scanner in = new Scanner(System.in);
         System.out.print("If u want to play write 2 if u want to leave write 1:");             
         int LeaveOrNo = in.nextInt();
+        int Try=0;
         while(LeaveOrNo==2){
         // TODO code application logic here
         Random random=new Random();
@@ -29,12 +30,21 @@ public class Game {
         int Lucku = in.nextInt(); 
                  
         if(RandomNumber==Lucku){
-            System.out.println("You Win");     
+            System.out.println("You Win");    
+            break;
        }
         else{
+            
           System.out.println("New Number u failed:"+Lucku);
-
+            Try++;
+            System.out.println("Try:"+Try);
         }
+        if( Try == 3) {
+
+                      System.out.println("YOU FAILED");
+                      break;
+        }
+        
         }
 } 
 }
